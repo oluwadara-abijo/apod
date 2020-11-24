a93c703e1a05b18d47c3b730968bbb124666fc0a
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2020-11-24 09:20:07 +0100

    feat: Display all pictures

--- app/src/main/java/com/dara/apod/MainActivity.kt
+++ app/src/main/java/com/dara/apod/MainActivity.kt
@@ -2,20 +2,30 @@ package com.dara.apod
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.dara.apod.model.Picture
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var pictures: List<Picture>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val data = this.readJsonFile("data.json")
-        tv_hello.text = data
+        pictures = jsonToObject(data)
+        setupRecyclerview()
 
-        val pictures = jsonToObject<List<Picture>>(data)
-        println("Result - $pictures")
+    }
 
-
+    fun setupRecyclerview() {
+        val gridlayoutManager = GridLayoutManager(this, 2)
+        val pictureAdapter = PictureAdapter(pictures, this)
+        rv_pictures.apply {
+            layoutManager = gridlayoutManager
+            adapter = pictureAdapter
+        }
     }
 }
